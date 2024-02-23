b510bc70c0c6c92e3cc7c870f31504e365f2bd26
Dirk Moreno Arias <dirk.moreno73@gmail.com>
2024-02-24 07:25:13 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}
+      <SpeedInsights />
+      <Analytics />
+      </body>
     </html>
   );
 }
